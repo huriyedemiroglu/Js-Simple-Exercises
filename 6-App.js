0b6c708fcfb649a9a2b36f57da87dea6687a40f9
huriyedemiroglu <demirogluhu@gmail.com>
2022-05-19 21:09:45 -0700

    added

---
+++ 6-App.js
@@ -0,0 +1,12 @@
+// factorial//
+
+const firstFactorial =(num)=>{
+  let factorial =1
+  for (let i = 2; i<=num; i++){
+    factorial *= i
+
+  }
+  return factorial
+}
+console.log(firstFactorial(5));
+
