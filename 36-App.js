869eb33b6764d90c2e96d877836de91610dc7432
huriyedemiroglu <demirogluhu@gmail.com>
2022-05-30 15:34:57 -0700

    added

---
+++ 36-App.js
@@ -0,0 +1,12 @@
+// simple adding//
+
+const simpleAdding =(num)=>{
+  
+  let result = 0
+  for(let i = 1 ; i <= num; i++){
+    result = result + i
+  }
+    return result
+};
+
+console.log(simpleAdding(13));
