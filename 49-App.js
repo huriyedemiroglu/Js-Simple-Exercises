babecc77e485536da4d453b65d6478064b77f642
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2022-10-17 23:09:18 -0500

    added

---
+++ 49-App.js
@@ -0,0 +1,17 @@
+// Vowel Count
+// Using the JS langiage, have the funct VowelCount(str)
+// take the str string parameter being passed and return the Numberof vowels the string contains
+
+const vowelCount=(str)=>{
+//const arr = str.split('') == [...str]
+  let i = 0;
+   [...str].forEach((letter)=>{
+       if((/[a,e,i,o,u,A,E,I,O,U]/).test(letter)){
+            i = i+1
+       }
+ })
+
+ return i
+
+}
+console.log(vowelCount("All cows eat grass"));
