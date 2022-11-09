f410c913573983a5ec9586c8d807e99c615ae038
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2022-11-09 09:39:02 -0600

    added

---
+++ 54-App.js
@@ -0,0 +1,11 @@
+// assesment //
+function make(minimum,maximum){
+const verify=(inputValue)=>{
+  if (inputValue < minimum) return "Input is less than minimum value"
+  if (inputValue > maximum) return "Input is more than maximum value"
+  return 'Input is in range'
+}
+return verify
+}
+
+console.log( make('20'))
