7468d7aa78fe97945827ebc41a95af5b73fa93e1
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2022-11-10 10:30:18 -0600

    added

---
+++ 55-App.js
@@ -0,0 +1,12 @@
+// find a maximum number //
+
+let number = maximum(150,2500)
+console.log(number)
+
+function maximum(number1,number2){
+  if(number1 > number2)
+  return number1;
+  return number2
+}
+
+//console.log(maximum(150,2500))
---
+++ 56-App.js
@@ -0,0 +1,45 @@
+// // first method
+// const arrayAddition=(arr)=>{
+//  const newArr =[...arr]
+// let sum = 0
+// const maxValue = Math.max(...arr)
+//   //ex console.log(Math.max(1,3,2))
+//   // const array=[1,2,3] asagidaki gibi convert edilir
+//   //console.log(Math.max(...array))
+// newArr.splice(arr.indexOf(maxValue),1)
+ 
+// for(let i = 0; i < newArr.length; i++){
+//   sum +=newArr[i]
+// }
+// return maxValue === sum ? true : false
+// }
+// console.log(arrayAddition([4,6,23,10,1,3]))
+//////////////////////////////////////////////////////////////////////////////////
+// // second method //
+// const arrayAddition=(arr)=>{
+//   arr.sort((a,b)=>a-b)
+//   let sum=0
+//   const maxValue= arr[arr.length-1]
+ 
+//   for(let i = 0; i<arr.length-1; i++){
+//     sum += arr[i]
+//   }
+
+// return maxValue === sum ? true : false
+// }
+// console.log(arrayAddition([4,6,23,10,1,2]))
+///////////////////////////////////////////////////////////////////////////////
+// third method //
+const arrayAddition=(arr)=>{
+
+  const maxValue = Math.max(...arr)
+  let sum = 0
+
+  for(let i =0 ; arr.length ;i++){
+    sum += arr[i]
+  }
+return sum/2 === maxValue ? true : false
+
+}
+console.log(arrayAddition([4,6,23,10,1,2]))
+
