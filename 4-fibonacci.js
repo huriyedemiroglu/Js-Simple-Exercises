9103322e797d2a4a8b2dd8d24671eaaed31480a0
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-12-05 12:20:42 -0600

    up

--- 4-fibonacci.js
+++ 4-fibonacci.js
@@ -1,13 +1,17 @@
 // /*fibonacci*/ 0 1 1 2 3 5 8 13 21 34 55 89 144
+///////////////////////////////////////////////
 
-function fibonacci(n){
-  let result=[0,1]
-for(let i= 2; i<=n; i++){
-  result.push(result[i-2]+result[i-1]);
-}
-return result;
-}
-console.log(fibonacci(10));
+// function fibonacci(n){
+//   let result=[0,1]
+// for(let i= 2; i<=n; i++){
+//   result.push(result[i-2]+result[i-1]);
+// }
+// return result;
+// }
+// console.log(fibonacci(10));
+
+
+/////////////////////////////////////////////////
 
 
 // recursive solution
@@ -21,3 +25,22 @@ console.log(fibonacci(10));
 //   return fibonacci(n-1) + fibonacci(n-2)
 // }
 // console.log (fibonacci(7))
+
+
+/////////////////////////////////////////////////////
+
+// Dinamic solution with memoization
+
+const fibonacci=(n,prev=[])=>{
+  let result
+
+  if( n == 0){
+    return 0
+  } else if (n == 1){
+    return 1
+  }
+  result = fibonacci(n-1, prev) + fibonacci(n-2, prev)
+  
+  return result
+}
+console.log(fibonacci(6))
