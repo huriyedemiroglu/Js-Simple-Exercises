70ec9a71d2a1b6dd3662ea2be733f649c0bb04d5
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-12-11 14:12:34 -0600

    up

--- 66-dashInsert.js
+++ 66-dashInsert.js
@@ -2,9 +2,21 @@
 // Don't count zero as an odd or even number
  const dashInsert2 = (num)=>{
    const numStr = num.toString()
+   let newStr = ""
  
-   for(let i = 0 ; i<numStr.length; i++)
- 
- 
- return numStr
+   for(let i = 0 ; i < numStr.length ; i++){
+    if(numStr[i]!=0 && numStr[i+1] !=0 && numStr[i] % 2 === 0 && numStr[i+1] %2 === 0 &&
+      (i+1) < numStr.length){
+        newStr += numStr[i] + "*"
+      }else if(numStr[i]!=0 && numStr[i+1] !=0 && numStr[i] % 2 !=0 && numStr[i+1] %2 !=0 &&
+        (i+1) < numStr.length){
+          newStr += numStr[i] + "-"
+        } else{
+          newStr += numStr[i]
+
+        }
+   }
+ return newStr
   }
+  console.log(dashInsert2(4546793))
+  console.log(dashInsert2(123555888))
