06d31bf78652d41dabbc06c8cce04e609689f90c
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-12-11 14:03:29 -0600

    coderbyte challange

---
+++ 66-dashInsert.js
@@ -0,0 +1,10 @@
+// for example: if str is 4546793 the output sholud be 454*67-9-3.
+// Don't count zero as an odd or even number
+ const dashInsert2 = (num)=>{
+   const numStr = num.toString()
+ 
+   for(let i = 0 ; i<numStr.length; i++)
+ 
+ 
+ return numStr
+  }
